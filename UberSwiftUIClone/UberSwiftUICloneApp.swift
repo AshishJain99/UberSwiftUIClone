591f788524341d06e6b58e3c079652d9e306fe1c
Ashish Jain <imjainashish@gmail.com>
2024-05-19 09:52:22 +0530

    Initial Commit

---
+++ UberSwiftUIClone/UberSwiftUICloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberSwiftUICloneApp.swift
+//  UberSwiftUIClone
+//
+//  Created by Ashish Jain on 19/05/24.
+//
+
+import SwiftUI
+
+@main
+struct UberSwiftUICloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
